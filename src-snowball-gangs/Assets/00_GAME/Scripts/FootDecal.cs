fcd3d6826f67f2ebee7d988af91a6a6c7da81254
seed value <snpxa@ya.ru>
2023-01-15 11:38:49 +0300

    script uses fixes. Materials...etc

--- src-snowball-gangs/Assets/00_GAME/Scripts/FootDecal.cs
+++ src-snowball-gangs/Assets/00_GAME/Scripts/FootDecal.cs
@@ -2,11 +2,11 @@ using NTC.Global.Pool;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
+//using UnityEngine.Rendering.Universal;
 using NTC.Global.Cache;
 using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class FootDecal : MonoCache
 {
-    [SerializeField] DecalProjector _decal;
+   // [SerializeField] DecalProjector _decal;
 }
